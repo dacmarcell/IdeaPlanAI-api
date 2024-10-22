b63c712f1da2acff407954592ae9587aab369566
dacmarcell <dacmarcell@gmail.com>
2024-10-22 11:39:05 -0300

    feat: Add Webservice class for creating an Express server
    This commit adds a new file, `webservice.ts`, which contains the implementation of the `Webservice` class. The `Webservice` class is responsible for creating an Express server with basic configuration and starting it on a specified host and port. It also provides methods for creating POST endpoints.
    
    The `Webservice` class is intended to be used as a utility for setting up a web service in the application.
    
    Co-authored-by: [Author Name]

---
+++ src/lib/webservice.ts
@@ -0,0 +1,30 @@
+import express from "express";
+
+export class Webservice {
+  express: express.Express;
+  host: string;
+  port: number;
+
+  constructor(host?: string, port?: number) {
+    this.host = host ?? "localhost";
+    this.port = port ?? 3000;
+    this.start();
+  }
+
+  private start() {
+    this.express = express();
+    this.express.use(express.json());
+
+    this.express.listen(this.port, () => {
+      console.info(`Server started at http://${this.host}:${this.port}`);
+    });
+  }
+
+  getInstance() {
+    return this.express;
+  }
+
+  createPost(endpoint: string, callback: (req: any, res: any) => void) {
+    this.express.post(endpoint, callback);
+  }
+}
