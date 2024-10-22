e0e462daeb9a2ea29ed8b2ef2a1e876e7ba872a4
dacmarcell <dacmarcell@gmail.com>
2024-10-21 22:18:06 -0300

    chore: generate prompt and send data to ai

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,21 @@ import OpenAI from "openai";
 import { env } from "./config/env.ts";
 import { data } from "./config/constants.ts";
 
+import { PromptGenerator } from "./lib/promptGenerator.ts";
+
 const openai = new OpenAI({
   apiKey: env.NVIDIA_NIM_API_KEY,
   baseURL: data.url,
 });
 
 export async function main() {
+  const content = PromptGenerator.generate(
+    "Desenvolver um aplicativo de gerenciamento de tarefas para equipes de desenvolvimento de software."
+  );
+
   const completion = await openai.chat.completions.create({
     model: data.model,
-    messages: [{ role: "user", content: "Olá como estás?" }],
+    messages: [{ role: "user", content }],
     temperature: data.temperature,
     top_p: data.topP,
     max_tokens: data.maxTokens,
