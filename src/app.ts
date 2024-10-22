acae9aa6c3715f809baca93df44870003053b0b7
dacmarcell <dacmarcell@gmail.com>
2024-10-21 21:36:57 -0300

    chore: fetch nemotron model

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import OpenAI from "openai";
+import { env } from "./config/env.ts";
+import { data } from "./config/constants.ts";
+
+const openai = new OpenAI({ apiKey: env.API_KEY, baseURL: data.url });
+
+export async function main() {
+  const completion = await openai.chat.completions.create({
+    model: data.model,
+    messages: [
+      {
+        role: "user",
+        content: "Olá como estás?",
+      },
+    ],
+    temperature: data.temperature,
+    top_p: data.topP,
+    max_tokens: data.maxTokens,
+    stream: true,
+  });
+
+  for await (const chunk of completion) {
+    process.stdout.write(chunk.choices[0]?.delta?.content ?? "");
+  }
+}
+
+main();
