4e9d59e66dacb5eda97526b43a8353edc8275477
dacmarcell <dacmarcell@gmail.com>
2024-10-22 01:15:33 -0300

    refactor: Refactor app.ts to use OpenAINemotron class for interacting with the OpenAI API

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,15 @@
-import OpenAI from "openai";
-
 import { env } from "./config/env.ts";
-import { data } from "./config/constants.ts";
 
 import { PromptGenerator } from "./lib/promptGenerator.ts";
+import { OpenAINemotron } from "./lib/openAiNemotron.ts";
 
-const openai = new OpenAI({
-  apiKey: env.NVIDIA_NIM_API_KEY,
-  baseURL: data.url,
-});
-
-export async function main() {
+async function main() {
   const content = PromptGenerator.generate(
     "Desenvolver um aplicativo de gerenciamento de tarefas para equipes de desenvolvimento de software."
   );
 
-  const completion = await openai.chat.completions.create({
-    model: data.model,
-    messages: [{ role: "user", content }],
-    temperature: data.temperature,
-    top_p: data.topP,
-    max_tokens: data.maxTokens,
-    stream: true,
-  });
-
-  for await (const chunk of completion) {
-    process.stdout.write(chunk.choices[0]?.delta?.content ?? "");
-  }
+  const openai = new OpenAINemotron(env.NVIDIA_NIM_API_KEY);
+  await openai.sendMessage(content);
 }
 
 main();
