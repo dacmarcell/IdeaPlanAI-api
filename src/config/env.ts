3fc535c640cae243bf768aa194209acc9a37991c
dacmarcell <dacmarcell@gmail.com>
2024-10-23 03:31:23 -0300

    Refactor env.ts to include PORT variable in the environment configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,9 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const NVIDIA_NIM_API_KEY = process.env.NVIDIA_NIM_API_KEY;
+const PORT = parseInt(process.env.PORT);
 
-if (!process.env.NVIDIA_NIM_API_KEY) {
+if (!NVIDIA_NIM_API_KEY) {
   throw new RequiredInDotEnv("NVIDIA_NIM_API_KEY");
 }
+if (!PORT || isNaN(PORT)) {
+  throw new RequiredInDotEnv("PORT");
+}
 
-export const env = { NVIDIA_NIM_API_KEY: NVIDIA_NIM_API_KEY! };
+export const env = { NVIDIA_NIM_API_KEY, PORT };
