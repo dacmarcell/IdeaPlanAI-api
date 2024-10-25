b454fdf56ba3afec95540fa3fbb92b77e8017535
dacmarcell <dacmarcell@gmail.com>
2024-10-25 18:51:00 -0300

    Refactor app.ts to improve code organization and readability

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,48 @@ import { Webservice } from "./webservice/webservice.ts";
 async function main() {
   const api = new Webservice({ port: env.PORT });
 
+  const generatePrompts = ({ text }: { text: string }) => {
+    const categoryPrompt = PromptGenerator.generateCategory({ text });
+    const planPrompt = PromptGenerator.generatePlan({ text });
+
+    return { categoryPrompt, planPrompt };
+  };
+
+  const sendMessagesToNemotron = async ({
+    apiKey,
+    planPrompt,
+    categoryPrompt,
+  }: {
+    apiKey: string;
+    planPrompt: string;
+    categoryPrompt: string;
+  }) => {
+    const nemotron = new OpenAINemotron({ apiKey });
+
+    const category = await nemotron.sendMessage(categoryPrompt);
+    const plan = await nemotron.sendMessage(planPrompt);
+
+    return { category, plan };
+  };
+
+  const formatText = ({ text }: { text: string }) => text.trim();
+
   api.createPostEndpoint("/", async (req, res) => {
     const { text } = req.body;
 
-    const input = PromptGenerator.generate({ text: text.trim() });
+    const formattedText = formatText({ text });
 
-    const nemotron = new OpenAINemotron({ apiKey: env.NVIDIA_NIM_API_KEY });
-    const output = await nemotron.sendMessage(input);
+    const { categoryPrompt, planPrompt } = generatePrompts({
+      text: formattedText,
+    });
 
-    res.send(output);
+    const { category, plan } = await sendMessagesToNemotron({
+      planPrompt,
+      categoryPrompt,
+      apiKey: env.NVIDIA_NIM_API_KEY,
+    });
+
+    res.send({ category, plan });
   });
 }
 
