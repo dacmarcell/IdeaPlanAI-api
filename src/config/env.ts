2297ac2d1f667b85f579d1518a41c13cce96f8d6
dacmarcell <dacmarcell@gmail.com>
2024-10-25 18:51:58 -0300

    Refactor env.ts to include DATABASE_URL in the environment configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,12 +5,20 @@ dotenv.config();
 
 const NVIDIA_NIM_API_KEY = process.env.NVIDIA_NIM_API_KEY;
 const PORT = process.env.PORT ?? 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 if (!NVIDIA_NIM_API_KEY) {
   throw new RequiredInDotEnv("NVIDIA_NIM_API_KEY");
+}
+if (!DATABASE_URL) {
+  throw new RequiredInDotEnv("DATABASE_URL");
 }
 if (typeof PORT === "string" && isNaN(parseInt(PORT))) {
   throw new RequiredInDotEnv("PORT"); //FIXME: modificar erro
 }
 
-export const env = { NVIDIA_NIM_API_KEY, PORT: PORT as number };
+export const env = {
+  DATABASE_URL,
+  NVIDIA_NIM_API_KEY,
+  PORT: PORT as number,
+};
