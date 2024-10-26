ea032b7908cd65b5722b519bba9d2d659a59a8a8
dacmarcell <dacmarcell@gmail.com>
2024-10-26 00:30:19 -0300

    Refactor env.ts to include DATABASE_URL in the environment configuration and handle invalid PORT type

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,24 +1,27 @@
+import { InvalidType } from "../errors/InvalidType.ts";
 import { RequiredInDotEnv } from "../errors/RequiredInDotEnv.ts";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const NVIDIA_NIM_API_KEY = process.env.NVIDIA_NIM_API_KEY;
-const PORT = process.env.PORT ?? 3000;
+const PORT = Number(process.env.PORT);
 const DATABASE_URL = process.env.DATABASE_URL;
+const NVIDIA_NIM_API_KEY = process.env.NVIDIA_NIM_API_KEY;
 
 if (!NVIDIA_NIM_API_KEY) {
   throw new RequiredInDotEnv("NVIDIA_NIM_API_KEY");
 }
+
 if (!DATABASE_URL) {
   throw new RequiredInDotEnv("DATABASE_URL");
 }
-if (typeof PORT === "string" && isNaN(parseInt(PORT))) {
-  throw new RequiredInDotEnv("PORT"); //FIXME: modificar erro
+
+if (isNaN(PORT)) {
+  throw new InvalidType({ name: "PORT", type: "number" });
 }
 
 export const env = {
+  PORT,
   DATABASE_URL,
   NVIDIA_NIM_API_KEY,
-  PORT: PORT as number,
 };
