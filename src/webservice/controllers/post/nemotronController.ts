1fa3a6fb0f2ab9cc869d752a452093cc8b67b98e
dacmarcell <dacmarcell@gmail.com>
2024-11-10 03:42:28 -0300

    refactor: simplify NemotronController by removing unused methods and enhancing audio parsing

--- src/webservice/controllers/post/nemotronController.ts
+++ src/webservice/controllers/post/nemotronController.ts
@@ -1,51 +1,30 @@
-import { Request, Response } from "express";
-import { PromptGenerator } from "../../../prompt/promptGenerator";
-import { env } from "../../../config/env";
-import { OpenAINemotron } from "../../../nemotron/openAINemotron";
+import * as express from "express";
+
+import { env } from "../../../config/env.ts";
+import { PromptGenerator } from "../../../prompt/promptGenerator.ts";
+import { OpenAINemotron } from "../../../nemotron/openAINemotron.ts";
 
 export class NemotronController {
-  promptGenerator: PromptGenerator;
+  async parseAudioToText(req: express.Request, res: express.Response) {
+    const { file } = req.body;
 
-  constructor() {
-    this.promptGenerator = new PromptGenerator();
+    const nemotron = new OpenAINemotron({ apiKey: env.NVIDIA_NIM_API_KEY });
+    const text = await nemotron.transcribeAudio(file);
+
+    res.send({ text });
   }
 
-  async sendMessagesToNemotron(req: Request, res: Response) {
+  async sendMessagesToNemotron(req: express.Request, res: express.Response) {
     const { text } = req.body;
 
     const formattedText = this.formatText({ text });
+    const planPrompt = PromptGenerator.generatePlan({ text: formattedText });
 
-    const { planPrompt } = this.generatePrompts({
-      text: formattedText,
-    });
-
-    const { plan } = await this.sendMessages({
-      planPrompt,
-      apiKey: env.NVIDIA_NIM_API_KEY,
-    });
+    const nemotron = new OpenAINemotron({ apiKey: env.NVIDIA_NIM_API_KEY });
+    const plan = await nemotron.sendMessage(planPrompt);
 
     res.send({ plan });
   }
-
-  generatePrompts = ({ text }: { text: string }) => {
-    const planPrompt = PromptGenerator.generatePlan({ text });
-
-    return { planPrompt };
-  };
-
-  sendMessages = async ({
-    apiKey,
-    planPrompt,
-  }: {
-    apiKey: string;
-    planPrompt: string;
-  }) => {
-    const nemotron = new OpenAINemotron({ apiKey });
-
-    const plan = await nemotron.sendMessage(planPrompt);
-
-    return { plan };
-  };
 
   formatText = ({ text }: { text: string }) => text.trim();
 }
