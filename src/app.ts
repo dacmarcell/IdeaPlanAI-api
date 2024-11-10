1fcfc4ea3f06e41a8f24e9679a874b47ed6ecdb6
dacmarcell <dacmarcell@gmail.com>
2024-11-10 02:13:22 -0300

    refactor: integrate PostController for handling message processing in main app

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,18 @@
 import { env } from "./config/env.ts";
 
-import { PromptGenerator } from "./prompt/promptGenerator.ts";
-import { OpenAINemotron } from "./nemotron/openAINemotron.ts";
 import { Webservice } from "./webservice/webservice.ts";
+import { PostController } from "./webservice/controllers/post.ts";
 
 main();
-
 async function main() {
   const webservice = new Webservice({ port: env.PORT });
+  const postController = new PostController();
+
   const instance = webservice.getInstance();
 
   instance.post("/", async (req, res) => {
-    const { text } = req.body;
-
-    const formattedText = formatText({ text });
-
-    const { planPrompt } = generatePrompts({
-      text: formattedText,
-    });
-
-    const { plan } = await sendMessagesToNemotron({
-      planPrompt,
-      apiKey: env.NVIDIA_NIM_API_KEY,
-    });
-
-    res.send({ plan });
+    await postController.sendMessagesToNemotron(req, res);
   });
+
+  instance.post("/get-audio-in-text", async (req, res) => {});
 }
-
-const generatePrompts = ({ text }: { text: string }) => {
-  const planPrompt = PromptGenerator.generatePlan({ text });
-
-  return { planPrompt };
-};
-
-const sendMessagesToNemotron = async ({
-  apiKey,
-  planPrompt,
-}: {
-  apiKey: string;
-  planPrompt: string;
-}) => {
-  const nemotron = new OpenAINemotron({ apiKey });
-
-  const plan = await nemotron.sendMessage(planPrompt);
-
-  return { plan };
-};
-
-const formatText = ({ text }: { text: string }) => text.trim();
